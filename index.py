14f71cfc4ae9cdd629b299b4611ad64757da5e90
Hugo Danet <61951279+cartesis07@users.noreply.github.com>
2022-09-14 13:28:31 +0200

    datetime conversion

--- index.py
+++ index.py
@@ -1,13 +1,9 @@
 import pandas_datareader.data as web
-from pandas.io.json import json_normalize
 import datetime
 import pandas as pd 
-import io
 import requests
 
-import urllib3
-http = urllib3.PoolManager()
-import json
+from functools import reduce
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -31,18 +27,14 @@ import pandas as pd
 
 etf_list = pd.read_csv('etf_list.csv')
 
-# print(etf_list.to_string())
+print(etf_list.to_string())
 
 # get etf history with marketstack
-
 def get_etf(ticker):
     request = requests.get('http://api.marketstack.com/v1/eod?access_key=87a17751375d57c0a6dcecbfd9181246&symbols=' + ticker).json()
-    df  = pd.DataFrame.from_dict(request['data'])
-    return df[['date','open']]
+    df  = pd.DataFrame.from_dict(request['data'])[['date','close']]
+    df['date'] = pd.to_datetime(df['date']).dt.date
+    return df
 
 asia = get_etf("AEJ.XPAR")
 print(asia.head())
- 
-# https://swapi.dev/api/starships/9/ example url
-
-# def get_etf(ticker):
