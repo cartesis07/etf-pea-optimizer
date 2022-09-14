954aee343748368cac9fd63af9748f14a2d9b9ba
Hugo Danet <61951279+cartesis07@users.noreply.github.com>
2022-09-14 15:25:53 +0200

    pyportfolioopt implementation

--- portfolio.py
+++ portfolio.py
@@ -1,7 +1,37 @@
 import pandas as pd 
 
-portfolio = pd.read_csv("portfolio.csv")
+portfolio = pd.read_csv("portfolio.csv", parse_dates=True, index_col="date")
+column_means = portfolio.mean()
+portfolio = portfolio.fillna(column_means)
 
 print(portfolio.head())
 
-print(portfolio.isna().sum())
+# count nan values inside portfolio
+#print(portfolio.isna().sum())
+
+from pypfopt.expected_returns import mean_historical_return
+from pypfopt.risk_models import CovarianceShrinkage
+
+mu = mean_historical_return(portfolio)
+S = CovarianceShrinkage(portfolio).ledoit_wolf()
+
+from pypfopt.efficient_frontier import EfficientFrontier
+
+ef = EfficientFrontier(mu, S)
+weights = ef.max_sharpe()
+
+cleaned_weights = ef.clean_weights()
+print(dict(cleaned_weights))
+
+ef.portfolio_performance(verbose=True)
+
+
+from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
+
+latest_prices = get_latest_prices(portfolio)
+
+da = DiscreteAllocation(weights, latest_prices, total_portfolio_value=100000)
+
+allocation, leftover = da.greedy_portfolio()
+print("Discrete allocation:", allocation)
+print("Funds remaining: ${:.2f}".format(leftover))
